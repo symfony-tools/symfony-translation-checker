0cc7970d63d769d8c7692befa292b2e0961a68c3
Nyholm <tobias.nyholm@gmail.com>
2024-02-04 15:49:50 -0800

    Issue body syntax fix

--- src/Command/OpenIssuesCommand.php
+++ src/Command/OpenIssuesCommand.php
@@ -61,9 +61,9 @@ final class OpenIssuesCommand extends Command
         /** @var MissingTranslation $missingTranslation */
         foreach ($componentCollection as $missingTranslation) {
             if ($missingTranslation->getMissingCount() > 0) {
-                $link = sprintf('[%s](https://github.com/symfony/symfony/blob/%s/%s)', $missingTranslation->getFile(), $targetBranch, $missingTranslation->getFile());
-                $files .= '- '.$link.\PHP_EOL;
-                $details .= '####'.$link.\PHP_EOL;
+                $files .= sprintf('- [%s](https://github.com/symfony/symfony/blob/%s/%s)', $missingTranslation->getFile(), $targetBranch, $missingTranslation->getFile()).\PHP_EOL;
+
+                $details .= '####'.$missingTranslation->getFile().\PHP_EOL.\PHP_EOL;
                 $details .= '| Id | English | Translation | Status |'.\PHP_EOL;
                 $details .= '| -- | -- | -- | -- |'.\PHP_EOL;
                 foreach ($missingTranslation->getMissingTranslations() as $missing) {
@@ -88,7 +88,7 @@ These are the files that should be updated:
 $files
 
 <details>
-<summary>Show strings not translated</summary>
+<summary>Show related strings</summary>
 $details
 </details>
 
